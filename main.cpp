ac79a438423f518f3931af711a4e6c7be873cbb4
Felipe Torrezan <felipe.torrezan@iar.com>
2022-08-22 12:13:57 +0200

    Deduplified print()

--- main.cpp
+++ main.cpp
@@ -8,9 +8,14 @@ class Demo {
 public:
   Demo() = default;
   ~Demo() = default;
-  
+
   void set(uint8_t i, uint8_t v) { this->whatever[i] = v; }
-  uint8_t get(uint8_t index) { return whatever[index]; }
+  void print() {
+    cout << "whatever[0]=" << hex << static_cast<int>(this->whatever[0]) << endl;
+    cout << "whatever[1]=" << hex << static_cast<int>(this->whatever[1]) << endl;
+    cout << "whatever[2]=" << hex << static_cast<int>(this->whatever[2]) << endl;
+    cout << "whatever[3]=" << hex << static_cast<int>(this->whatever[3]) << endl;
+  }
 
   __root static uint8_t whatever[20] @ "System_Mailbox";
 };
@@ -19,16 +24,18 @@ __no_init uint8_t Demo::whatever[20];
 
 void main()
 {
-  Demo d;
-  
-  d.set(0, 0x41);
-  d.set(1, 0x42);
-  d.set(2, 0x43);
-  
-  cout << "whatever[0]=" << hex << static_cast<int>(d.get(0)) << endl;
-  cout << "whatever[1]=" << hex << static_cast<int>(d.get(1)) << endl;
-  cout << "whatever[2]=" << hex << static_cast<int>(d.get(2)) << endl;
-  cout << "whatever[3]=" << hex << static_cast<int>(d.get(3)) << endl;
-  
+  Demo d1,d2;
+
+  d1.set(0, 0x41);
+  d1.set(1, 0x42);
+  d1.set(2, 0x43);
+  d1.print();
+
+  d2.set(0, 0x14);
+  d2.set(1, 0x24);
+  d2.set(2, 0x34);
+  d2.print();
+
   while(1);
 }
+
